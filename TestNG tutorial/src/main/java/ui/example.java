e9c2534946724d2b127dc0296f06280aee839efa
vjasmitha <vjasmitha@vjasmitha-sa.stratapps.com>
2025-03-25 20:34:16 +0530

    test

---
+++ TestNG tutorial/src/main/java/ui/example.java
@@ -0,0 +1,16 @@
+package ui;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class example {
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://youtube.com");
+		Thread.sleep(1000);
+		driver.manage().window().maximize();
+
+	}
+
+}
